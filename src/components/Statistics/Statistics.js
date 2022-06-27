c8d8ec7698017fc56822bfc385474e7c17aaefce
VasylArtymovych <art777vasss@gmail.com>
2022-06-27 10:39:06 +0300

    update generateColor function

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -7,12 +7,14 @@ export default function statistics({title, stats}){
             {title && <h2 className={s.title}>{title}</h2>}
 
             <ul className={s.list}>
-                {stats.map(({id, label, percentage}) => (
-                    <li className={s.item} key={id} style={{backgroundColor: `${rendomHex()}`}}>
+                {stats.map(({id, label, percentage}) => {
+                    const color = rendomHex();
+                    return (
+                    <li className={s.item} key={id} style={{backgroundColor: `${color}`}}>
                         <span className={s.label}>{label}</span>
                         <span className={s.percentage}>{percentage}%</span>
                     </li>
-                ))}
+                )})}
             </ul>
         </section>
     )
